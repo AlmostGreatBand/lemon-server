a731fb85c07e8ec6d3826f2e7512ebd811b3c41d
MaryLynJuana <mary_juana@ukr.net>
2020-11-20 19:22:56 +0200

    test(api): Add tests for server

--- test/server/serverTest.js
+++ test/server/serverTest.js
@@ -1,8 +1,51 @@
 'use strict';
 
-const assert = require('assert');
+const assert = require('assert').strict;
+const https = require('https');
+const fs = require('fs');
 
-assert.ok(true, "Must be true");
+const url = 'https://localhost:8000/';
+const options = {
+  ca: [ fs.readFileSync('cert.pem') ],
+};
+const paths = [ 'profile/', 'cards/', 'transactions/', 'registration/', 'profile' ];
+const tests = [
+  ['alegator', 'good_job', ''],
+  ['', '', '<h1>&#127819Login and password should be specified</h1>'],
+  ['alegator', '', '<h1>&#127819Login and password should be specified</h1>'],
+  ['', 'good_job', '<h1>&#127819Login and password should be specified</h1>'],
+  ['saym', 'onloh', '<h1>&#127819Incorrect login entered :(</h1>'],
+  ['alegator', 'bad_job', '<h1>&#127819Incorrect password entered :(</h1>'],
+];
 
-// TODO: Create some files hierarchy in this directory, test whatever can be tested.
-// Tests are run by 'npm test' command (see package.json).
+const testPath = path => {
+  for (const test of tests) {
+    const [ login, passwd, expected ] = test;
+    const params = `?login=${login}&password=${passwd}`;
+    const fullPath = url + path + params;
+    const req = https.get(fullPath, options, res => {
+      const { statusCode } = res;
+      if (statusCode === 200) return;
+      if (statusCode !== 403) assert.fail(`Request Failed.\nStatus Code: ${statusCode}`);
+      res.on('data', msg => {
+        assert.strictEqual(
+          msg.toString(), expected, `Wrong error msg, expected ${expected}, got ${msg}`
+        );
+        console.log(`Test successful: ${fullPath}`);
+        console.log(msg.toString() + '\n');
+      });
+    });
+    req.setTimeout(3000, () => assert.fail('Request timed out'));
+  }
+};
+
+paths.forEach(testPath);
+
+const wrongPath = 'gfdfsgdf/';
+https.get(url + wrongPath, options, res => {
+  const { statusCode } = res;
+  assert.strictEqual(
+    statusCode, 404, `Wrong status code, expected 404, got ${statusCode}`
+  );
+  console.log('Test successful: wrong path');
+}).setTimeout(3000, () => assert.fail('Request timed out'));
