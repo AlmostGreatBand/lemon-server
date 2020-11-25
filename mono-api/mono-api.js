41a529d3dcd2f11b2002f4d6ac6e83f6b53b90f7
NeProgramist <zaskozhenya@gmail.com>
2020-11-26 00:23:49 +0200

    feat: add getAccounts function

--- mono-api/mono-api.js
+++ mono-api/mono-api.js
@@ -1,26 +1,54 @@
 'use strict'; 
 
-const http = require('http');
-const BASE_URL = 'api.monobank.ua'
+const http = require('https');
+const BASE_URL = 'api.monobank.ua';
+const TOKEN = 'uMLmvCFm6kA4N8mrflxayXMcZPqFUrxpm9q_CxBsMkaY'
 
-const getTransactions = (account, from, to) => {
-    const options = {
+function createRequestBody(path, token) {
+    return {
         hostname: BASE_URL,
-        path: `/personal/statement/${account}/${from}`,
+        path: path,
         method: 'GET',
         headers: {
-            'X-Token': 'uMLmvCFm6kA4N8mrflxayXMcZPqFUrxpm9q_CxBsMkaY',
+            'X-Token': token,
             'Content-Type': 'application/json; charset=UTF-8',
         }
     };
-    let req = http.request(options)
-    req.on('response', res => res.on('data', body => console.log(body)))
-    req.on('error', err => console.log(err))
-    req.end()
 }
 
+function getData(path, token) {
+    const options = createRequestBody
+    req.on('response', res => {
+        res.statusCode // handler
+            res.on('data', body => {
+                try {
+                    // return to repo
+                    console.log(JSON.parse(body))
+                } catch(e) {
+                    console.log(`Error: something went wront while trying to parse data: ${e}`) 
+                }
+            });
+        }
+    );
+    req.on('error', err => {
+        // return to repo
+        console.log(JSON.parse(err))
+    }); 
+    req.end();
+}
 
+function getTransactions(token, account, from, to) {
+    let queryString = `/personal/statement/${account}/${from}`;
+    if (to) queryString += `/${to}`;
+    if (account !== undefined && from !== undefined) return getData(queryString, token)
+    else console.log("Error: account or from-date is not specified")
+}
+
+function getAccounts(token) {
+    const queryString = 'personal/client-info'
+    return getData(path, token)
+}
 // Usage
 
-let today = new Date()
-getTransactions(0, 1604000000000)
+// getTransactions(TOKEN, 0, 1604000000000);
+getAccounts(TOKEN)
