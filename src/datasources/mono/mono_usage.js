750ae42786198d27cf0230a0e507ddb515e6e0a9
NeProgramist <zaskozhenya@gmail.com>
2021-01-04 13:51:25 +0200

    feat(examples): add more examples of api-calls

--- src/datasources/mono/mono_usage.js
+++ src/datasources/mono/mono_usage.js
@@ -2,12 +2,39 @@
 
 const MonoDataSource = require('./mono.js')
 const monoDS = new MonoDataSource(process.env.MONO_TOKEN);
-(async () => {
+
+const getTransactions = async (account, from, to) => {
     try {
-        const transactions = await monoDS.getTransactions(undefined, new Date(2020, 10, 15), new Date(2020, 10, 26));
-        console.log(transactions) 
+        const transactions = await monoDS.getTransactions(account, from, to);
+        console.log(transactions)
     } catch(e) {
-        /* Do smth logic here */
-        console.log(e)
+        console.error(e)
     }
+}
+
+const getAccounts = async () => {
+    try {
+        const accounts = await monoDS.getAccounts();
+        console.log(accounts)
+    } catch(e) {
+        console.error(e)
+    }
+}
+
+(async () => {
+    // Scenario 1: get transactions 
+    console.log("---Scenario 1---")
+    await getTransactions(0, new Date(2020, 10, 15), new Date(2020, 10, 26))
+
+    // Scenario 2: get accounts
+    console.log("---Scenario 2---")
+    await getAccounts()
+
+    // Scenario 3: get transactions with invalid account
+    console.log("---Scenario 3---")
+    await getTransactions(-3, new Date(2020, 10, 15), new Date(2020, 10, 26))
+
+    // Scenario 4: get transactions without _to_ date
+    console.log("---Scenario 4---")
+    await getTransactions(0, new Date(2020, 11, 15))
 })();
