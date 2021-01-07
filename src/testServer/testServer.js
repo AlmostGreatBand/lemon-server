243fadecc305add604925e9a69b3e90860c868b2
ALEGATOR1209 <kovalyshinO@gmail.com>
2021-01-07 13:05:36 +0200

    Test server updated to support http & deployment to Heroku

--- src/testServer/testServer.js
+++ src/testServer/testServer.js
@@ -1,15 +1,31 @@
 'use strict';
 
 const https = require('https');
+const http = require('http');
 const fs = require('fs');
-const { URL } = require('url');
-
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-};
-
 const routing = require('./routing.js');
+
+http.name = 'http';
+https.name = 'https';
+
+const key = (() => {
+  try {
+    return fs.readFileSync('key.pem');
+  } catch (e) {
+    return undefined;
+  }
+})();
+
+const cert = (() => {
+  try {
+    return fs.readFileSync('cert.pem');
+  } catch (e) {
+    return undefined;
+  }
+})();
+
+const options = { key, cert };
+
 
 const normalizePath = pathname => (
   pathname.endsWith('/') ? pathname : pathname + '/'
@@ -32,11 +48,12 @@ const authorizeUser = (req, res) => {
   return credentialsASCII.split(':');
 };
 
-const server = https.createServer(options, (req, res) => {
+const protocol = key && cert ? https : http;
+
+const server = protocol.createServer(options, (req, res) => {
   const credentials = authorizeUser(req, res);
   if (!credentials) return;
-  const { pathname } = new URL(req.url, 'https://localhost:8000/');
-  const handler = routing[normalizePath(pathname)];
+  const handler = routing[normalizePath(req.url)];
   if (!handler) {
     res.writeHead(404);
     return res.end('<h1>&#127819Page not found :(</h1>');
@@ -47,5 +64,7 @@ const server = https.createServer(options, (req, res) => {
   res.end((typeof data === 'string') ? data : JSON.stringify(data));
 });
 
-server.listen(8000);
+server.listen(process.env.PORT || 8000);
 console.log('Test server goes brrrrrrrrrrrr');
+const address = { ...server.address(), protocol: protocol.name };
+console.dir(address);
