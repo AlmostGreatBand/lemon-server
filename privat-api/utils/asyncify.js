e1f13eaa18b7ad13270eae19d214042ff5026b8c
PaIIadium <vladislavkuchin2001@gmail.com>
2021-01-07 02:33:51 +0200

    restructuring

--- privat-api/utils/asyncify.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const asincify = fn => (...args) => new Promise(
-  (resolve, reject) => fn(...args, 
-    (err, data) => err === null ? resolve(data) : reject(err))
-);
-
-module.exports = asincify;
---
+++ src/datasources/privat24/utils/transactions.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const fs = require('fs');
+const xmlJs = require('xml-js');
+const transactionsXML = fs.readFileSync('./xml_data/transactions.xml', 'utf-8');
+
+const fillTransactionsXml = dataForTransactions => {
+  const xmlObj = xmlJs.xml2js(transactionsXML, {compact: true});
+
+  const xmlMerchant = xmlObj.request.merchant;
+  xmlMerchant.id._text = dataForTransactions.merchantId;
+
+  const xmlData = xmlObj.request.data;
+  xmlData.wait._text = dataForTransactions.wait;
+  xmlData.test._text = dataForTransactions.test;
+
+  const xmlPayment = xmlData.payment;
+  xmlPayment._attributes.id = dataForTransactions.paymentId;
+
+  const [xmlStartDate, xmlEndDate, xmlCardNumber] = xmlPayment.prop;
+  xmlStartDate._attributes.value = dataForTransactions.startDate;
+  xmlEndDate._attributes.value = dataForTransactions.endDate;
+  xmlCardNumber._attributes.value = dataForTransactions.cardNumber;
+
+  const xmlWithData = xmlJs.js2xml(xmlObj, {spaces: 0, compact: true, fullTagEmptyElement: true});
+  return xmlWithData;
+};
+
+module.exports = {fillTransactionsXml};
