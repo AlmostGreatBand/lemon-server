db5605ddb8369330931efbfb9bef117be3e54181
Mary Juana <43133437+MaryLynJuana@users.noreply.github.com>
2021-06-02 22:13:30 +0300

    Cover test server with tests (#20)
    * fix(core): Fix path to test server
    
    * test(api): Add tests for server
    
    * test(api): Rewrite server tests to check authentification
    
    * refactor(tests): Change array of arrays to object factory
    
    * build(api): Make test server exportable and move server starting to a separate file
    
    * test(api): Enable test file to start and stop server
    
    * fix(api): Correct error codes and messages returned from server
    
    * refactor(api): Get rid of using HTTP responce in routing module
    
    * refactor(api): Replace explicit object creation with factory call
    
    * fix(api): Check error field instead of checking HTTP code
    
    * fix(tests): Replace setTimeout with checking counter of unfinished requests for closing server
    
    * fix(tests): Remove unnecessary Atomics
    
    * refactor(tests): Move testing of one request into separate function
    
    * refactor(tests): Move checking credentials into getOptions function
    
    * fix(core): Add 'use strict'
    
    * test(api): Add checking response data for requests with code 200
    
    * refactor(tests): Remove unnecessary second parameter to createServer
    
    * refactor(tests): Add newline at the end of file
    
    * fix(api): Correct expected field names in routing and tests

--- test/server/serverTest.js
+++ test/server/serverTest.js
@@ -1,8 +1,157 @@
 'use strict';
 
-const assert = require('assert');
+const assert = require('assert').strict;
+const http = require('http');
+const fs = require('fs');
+const createServer = require('../../src/testServer/testServer.js');
 
-assert.ok(true, 'Must be true');
+const url = 'http://localhost:8000/';
+const paths = [ 'profile/', 'cards/', 'transactions/', 'profile' ];
+const expectedResults = {
+  'profile/': {
+    account_id: 1,
+    name: 'Oleh',
+    login: 'alegator',
+    password: 'good_job'
+  },
+  'cards/': {
+    cards: [{
+      card_id: 321,
+      bank_id: 353,
+      bank: 'Mono',
+      card_num: 1234,
+      type: 'black',
+      balance: 3333,
+      currency: 'UAH',
+      token: '65yjy45fdh'
+    }]
+  },
+  'transactions/': {
+    transactions: [{
+      transaction_id: 1,
+      card_id: 321,
+      amount: -1500,
+      type: 'Cafe',
+      date: '2020-10-12T10:25:19.833Z'
+    }]
+  },
+  'profile': {
+    account_id: 1,
+    name: 'Oleh',
+    login: 'alegator',
+    password: 'good_job'
+  },
+};
 
-// TODO: Create some files hierarchy in this directory, test whatever can be tested.
-// Tests are run by 'npm test' command (see package.json).
+const testCase = (credentials, code, expected) => (
+  { credentials, code, expected }
+);
+const tests = [
+  testCase('alegator:good_job', 200, ''),
+  testCase('Unauthorized', 401, 'You should authorize to access the site'),
+  testCase(':', 401, 'Login and password should be specified'),
+  testCase('alegator:', 401, 'Login and password should be specified'),
+  testCase(':good_job', 401, 'Login and password should be specified'),
+  testCase('saym:onloh', 403, 'Incorrect login or password :('),
+  testCase('alegator:bad_job', 403, 'Incorrect login or password :('),
+];
+
+let counter = 0;
+
+const getOptions = credentials => {
+  if (credentials === 'Unauthorized') return {};
+  const credentialsBase64 = Buffer.from(credentials).toString('base64');
+  let options = { 
+      headers: { 
+        Authorization: `Basic ${ credentialsBase64 }` 
+      }
+  };
+  return options;
+};
+
+const checkStatusCode = (statusCode, expectedCode, path, credentials) => {
+  assert.strictEqual(
+    statusCode, expectedCode,
+    `Wrong status code, expected ${expectedCode}, got ${statusCode}
+    Path: ${path}
+    Credentials: ${credentials}`
+  );
+};
+
+const checkMsg = (msg, expected, path, credentials) => {
+  assert.strictEqual(
+    JSON.parse(msg).error.toString(), `&#127819${expected}`, 
+    `Wrong error msg, expected &#127819${expected}, got ${msg}
+    Path: ${path}
+    Credentials: ${credentials}`
+  );
+};
+
+const checkResponseData = (data, path) => {
+  const expected = JSON.stringify(expectedResults[path]);
+  assert.strictEqual(data, expected, `Wrong response data, expected: 
+    ${expected}, 
+    got: 
+    ${data}
+    Path: ${path}`);
+};
+
+const logSuccess = (path, credentials, statusCode, responseData) => {
+  console.log(
+    `Test successful: ${path} ${credentials}, code: ${statusCode},
+    got response: ${responseData.toString()}\n`
+  );
+};
+
+const testRequest = async (path, test) => {
+  counter++;
+  const { credentials, code, expected } = test;
+  let options = getOptions(credentials);
+  const req = http.get(url + path, options, async res => {
+    const { statusCode } = res;
+    checkStatusCode(statusCode, code, path, credentials);
+    if (statusCode === 200) {
+      counter--;
+      const buffers = [];
+      for await (const chunk of res) buffers.push(chunk);
+      const data = Buffer.concat(buffers).toString();
+      checkResponseData(data, path);
+      logSuccess(path, credentials, statusCode, data);
+      if (counter <= 0) server.close(() => console.log('Tests finished'));
+      return;
+    };
+    res.on('data', msg => {
+      counter--;
+      checkMsg(msg, expected, path, credentials);
+      logSuccess(path, credentials, statusCode, msg);
+      if (counter <= 0) server.close(() => console.log('Tests finished'));
+    });
+  });
+  req.setTimeout(3000, () => assert.fail('Request timed out'));
+};
+
+const testPath = async path => {
+  for (const test of tests) {
+    await testRequest(path, test);
+  }
+};
+
+const testWrongPath = async () => {
+  const wrongPath = 'gfdfsgdf/';
+  const wrongPathTest = testCase(
+    'alegator:good_job', 404, 'Page not found :('
+  );
+  await testRequest(wrongPath, wrongPathTest);
+};
+
+const server = createServer(http);
+server.listen(8000);
+
+(async () => {
+  try {
+    await paths.forEach(testPath);
+    await testWrongPath();
+  } catch(err) {
+    console.error(err);
+  }
+})();
